439ae497d67e788e2565264c6f7657cdb5c00167
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-05-24 18:21:42 +0000

    variables in java

--- course/Hello.java
+++ course/Hello.java
@@ -1,5 +1,14 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int num = 3;
+        System.out.println(num);
+
+        int num1 = 5;
+        int num2 = 7;
+        int num3 = 6, num4 = 8;
+
+        System.out.println(num1 + num2);
+        System.out.println(num4 - num3);
     }
 }
