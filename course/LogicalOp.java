279c82b84be3b97adda1aadc3062cae615e4af10
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-05-25 12:47:47 +0000

    if else, ternary operator in java

--- course/LogicalOp.java
+++ course/LogicalOp.java
@@ -6,5 +6,29 @@ public class LogicalOp {
         System.out.println((x < y) || (a > b)); // Logical OR --> ||
         System.out.println(!(x < y)); // Logical NOT --> !
         System.out.println((x < y) ^ (a > b)); // XOR
+
+        // Let's learn about conditional statements
+        System.out.println("Let's start with conditional statements");
+
+        if(x > 10) {
+            System.out.println("Hello");
+        }
+        else {
+            System.out.println("Bye");
+        }
+
+        // Ternary Operator
+
+        int n = 4;
+        int result = 0;
+        if(n%2==0)
+            result = 10;
+        else
+            result = 20;
+
+        System.out.println(result);
+        
+        int result2 = n%2!=0 ? 10 : 20;
+        System.out.println(result2);
     } 
 }
