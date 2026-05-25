6419e380b843ad8b21798118f08f9117c2f8090c
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-05-25 12:16:49 +0000

    Logical operators details

--- course/LogicalOp.java
+++ course/LogicalOp.java
@@ -2,6 +2,9 @@ public class LogicalOp {
     public static void main(String ar[]){
         int x = 5, y = 6;
         int a = 8, b = 10;
-        System.out.println((x < y) && (a > b));
-    }
+        System.out.println((x < y) && (a > b)); // Logical AND --> &&
+        System.out.println((x < y) || (a > b)); // Logical OR --> ||
+        System.out.println(!(x < y)); // Logical NOT --> !
+        System.out.println((x < y) ^ (a > b)); // XOR
+    } 
 }
