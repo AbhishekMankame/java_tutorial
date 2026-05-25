93a21fa74193098b279d63054706c64e27c8a618
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-05-25 17:36:43 +0000

    do while loop

--- course/Loop.java
+++ course/Loop.java
@@ -1,10 +1,21 @@
 public class Loop {
     public static void main(String ar[]){
         int i=1;
+
+        // while loop
         while(i<=4){
             System.out.println("Hi "+i);
             i++;
         }
         System.out.println("Bye "+i);
+
+        // do while loop
+        int j=5;
+        do {
+            System.out.println("Hi "+j);
+            j++;
+        }
+        while(j<=4);
+
     }
 }
