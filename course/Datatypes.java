94b17e9a73935019a6354307e00206189d0a9b58
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-05-25 03:43:20 +0000

    datatypes in java

---
+++ course/Datatypes.java
@@ -0,0 +1,21 @@
+public class Datatypes {
+    public static void main(String a[]){
+        int num1 = 9;
+        byte by = 127;
+        short sh = 200;
+        long l = 5857l;
+        float f = 5.8f;
+        double d = 5.87;
+        char c = 'k';
+        boolean b = true;
+
+        System.out.println(num1);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(f);
+        System.out.println(d);
+        // System.out.println(num1); --> This is comment in Java
+        System.out.println(c);
+        System.out.println(b);
+    }
+}
