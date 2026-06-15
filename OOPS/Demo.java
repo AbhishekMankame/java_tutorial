c4aba4b59d51fe279491a04074432d6c0b0d536c
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-06-15 06:14:05 +0000

    calculator class update

---
+++ OOPS/Demo.java
@@ -0,0 +1,18 @@
+class Calculator
+{
+    public int add(){
+        System.out.println()
+    }
+}
+
+
+public class Demo {
+    public static void main(String a[]){
+        int num1 = 4;
+        int num2 = 5;
+
+        int result = num1 + num2;
+
+        System.out.println(result);
+    }
+}
