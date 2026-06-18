60ebacd021b95da7ea3552ce83916ee5b8f6b6e6
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-06-18 08:27:33 +0000

    strings update

--- OOPS/StringIntro.java
+++ OOPS/StringIntro.java
@@ -9,8 +9,12 @@ class StringIntro {
         System.out.println(name1);
         System.out.println(name.hashCode());
 
+        // hashCode() is a method that returns an integer hash value representing an object. It is mainly used by hash-based collections such as 'HashMap' and 'HashSet' to store and retrieve objects efficiently.
+        // For a 'String', the hash code is calculated based on the characters in the string, so equal strings produce the same hash code.
+
         // To concatenate string we can use '+' operator
         System.out.println("Hello " + name1);
+        System.out.println(name.concat(" Reddy"));
         System.out.println(name.charAt(1));
         System.out.println(name1.charAt(0));
     }
