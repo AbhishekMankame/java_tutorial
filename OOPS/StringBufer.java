7e5b30a23e9690ddc17f794cf3e3e1e582568266
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-06-18 13:47:27 +0000

    simple update

--- OOPS/StringBufer.java
+++ OOPS/StringBufer.java
@@ -4,5 +4,13 @@ public class StringBufer {
 
         // StringBuffer will have a string which is mutable. StringBuffer will give the buffer size of 16 bytes
         System.out.println(sb.capacity());
+        System.out.println(sb.length());
+        System.out.println();
+
+        StringBuffer sb2 = new StringBuffer("Abhishek");
+        System.out.println(sb2.capacity());
+        System.out.println(sb2.length());
+        System.out.println();
+
     }
 }
