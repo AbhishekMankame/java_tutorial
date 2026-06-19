6c29601398f8d68a86f68f3a455d7878dd69b00b
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-06-19 04:32:01 +0000

    constructor introduction

--- OOPS/Encapsulation1.java
+++ OOPS/Encapsulation1.java
@@ -13,6 +13,9 @@ class Human {
         this.age = age;
     }
 
+    // Note: Preference will always be given to the local variable
+    // 'this' keyword represents the current object
+
     public String getName() {
         return name;
     }
@@ -20,17 +23,36 @@ class Human {
     public void setName(String name){
         this.name = name;
     }
+
+    // Constructor: Constructor looks like method itself, that means constructor will have a name, round brackets.
+    // But the only change is for constructor we will not specify the return type
+
+    public Human() {
+        // System.out.println("In Constructor");
+        age = 12;
+        name = "John";
+    }
+
+    /*
+    Note: Constructor has a same name as that of the class name.
+    Constructor never returns anything.
+    Just specify the access, which is public and then mention the name (Here it is 'Human')
+    - Even if we do not call the constructor, it will be called
+    Imp: Everytime we create the object, it will call the constructor
+    
+    */
 }
 
 class Encapsulation1 {
     public static void main(String a[]) {
         Human obj = new Human();
+        // Human obj1 = new Human();
 
         System.out.println(obj.getName());
         System.out.println(obj.getAge());
 
         obj.setAge(30);
-        obj.SetName("Abhishek");
+        obj.setName("Abhishek");
         System.out.println(obj.getName());
         System.out.println(obj.getAge());
     }
