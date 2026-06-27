a5b1e3a61b79432c15db2aac5e5a84b90b385ec6
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-06-27 15:11:32 +0530

    Create NamingConvention class with naming conventions
    Add NamingConvention class with main method and comments on Java naming conventions.

---
+++ OOPS/NamingConvention.java
@@ -0,0 +1,18 @@
+public class NamingConvention {
+    public static void main(String a[]) {
+      
+    }
+}
+
+/* 
+
+Java follows the naming convention of camel casing.
+class and interface name starts with capital letter. Eg - Calc, Runable
+variable and method we use smaller letter. Eg - marks, show()
+For constants it will be in complete capital. Eg: PIE, BRAND
+
+For two or more letter words, we can use camel casing. Eg: showMyMarks()
+In some language people uses snake casing. Eg: show_my_marks()
+
+
+*/
