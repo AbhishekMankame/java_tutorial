227c89bf90693d6ba55710932657685b60b8506c
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-06-27 21:05:04 +0530

    Add documentation for Java threads
    Added documentation on threads in Java, explaining their purpose and benefits.

---
+++ OOPS/ThreadsIntro.java
@@ -0,0 +1,11 @@
+/*
+## Thread
+A Thread in Java is the smallest unit of execution within a process. It allows a program to perform multiple tasks concurrently, improving performance and responsiveness.
+
+Why use Threads>
+- Execute multiple tasks simultaneously
+- Improve CPU utilization
+- Handle background tasks (e.g., file downloads, database operations)
+- Build responsive GUI application
+
+*/
